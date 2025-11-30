b3db195104b1c5f8e233d5a22febc4116e813bbb
sakshamwithweb <websaksham39@gmail.com>
2025-11-30 11:02:46 +0530

    Homepage ready (90%)

--- components/Hero.js
+++ components/Hero.js
@@ -2,18 +2,18 @@
 
 import React from 'react'
 import { Globe } from './Globe'
-
+import { Button } from './ui/button'
 
 const Hero = () => {
-
-
     return (
-        <main className='h-[92vh] flex mt-[8vh] text-white bg-black'>
-            <div className='w-[40%] z-99'>
-                <h2>Lorem Ipsulum CHaracter Hero Nacbar with public dir</h2>
-            </div>
-            <div className='w-[60%]'>
-                <Globe />
+        <main className='h-[92vh] flex mt-[8vh] text-white bg-black relative'>
+            <Globe />
+            <div className='absolute z-10 top-6/12 left-6/12 -translate-6/12 flex flex-col items-center gap-8 pointer-events-none'>
+                <h2 className='text-5xl'>What if the internet understood you?</h2>
+                <div className='flex gap-4'>
+                    <Button className={" pointer-events-auto"}>Take a tour</Button>
+                    <Button className={"text-black pointer-events-auto"} variant={"outline"}>Skip</Button> {/* Make it later */}
+                </div>
             </div>
         </main>
     )
