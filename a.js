14c03947ea791057421613dd94246512838f1433
sakshamwithweb <websaksham39@gmail.com>
2026-01-26 19:05:06 +0530

    finally we are generating and storing embedded vectors in mongodb

--- a.js
+++
@@ -1,5 +0,0 @@
-console.log(typeof new Date().getFullYear())
-
-const compareVals = "2+"
-const [min, max] = compareVals.split("-").map((v) => parseInt(v))
-console.log(min, max)
--- app/api/authorized/githubFetch/route.js
+++ app/api/authorized/githubFetch/route.js
@@ -1,6 +1,8 @@
 import { checkAuthToken } from "@/lib/funcs/checkAuthToken"
 import { connectDB } from "@/lib/funcs/connectDB"
+import { generateEmbedding } from "@/lib/funcs/embedding"
 import { extractFeature, githubDataFetch } from "@/lib/funcs/github"
+import { buildEmbeddingText } from "@/lib/funcs/userDoc"
 import UserInfos from "@/lib/schema/userInfos"
 import { NextResponse } from "next/server"
 
@@ -13,17 +15,24 @@ export async function POST(req) {
         if (!response.success) return NextResponse.json({ success: false, error: response.error }, { status: response.status })
 
         const features = await extractFeature(data)
-        console.log(features)
+
+        // *********filter before embedding and all but for now directly embedding and searching as MVP
+
+        const embeddingText = buildEmbeddingText(features)
+
+        const embedding = await generateEmbedding(embeddingText)
 
         await connectDB()
 
-        // await UserInfos.updateOne({
-        //     username: response.userDetailsFromJwt?.payload?.username, "datas.type": "raw-online"
-        // }, {
-        //     "$push": {
-        //         "datas.$.data": data
-        //     }
-        // })
+        await UserInfos.updateOne({
+            username: response.userDetailsFromJwt?.payload?.username, "datas.type": "raw-online"
+        }, {
+            "$push": {
+                "datas.$.data": data
+            },
+            "features": features,
+            "embedding": embedding
+        })
         return NextResponse.json({ success: true, data })
     } catch (error) {
         console.log(error)
